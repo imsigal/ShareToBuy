76bd21423083b59c1d1884a838f1ea8cc7ccab4b
sigal <ssamburski@gmail.com>
2020-01-16 11:02:44 +0200

    add parse to project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Parse from 'parse'
+
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+Parse.initialize(
+  'RxNV8NwdLxj6ybkoL0AvcbgxXqfyRthKENFqWmQj', // This is your Application ID
+  '7CqXhbdXpQA6nJqp5cp5Ytwe27mrO449qWGqqnBW' // This is your Javascript key
+);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
