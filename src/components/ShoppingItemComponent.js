5a6ddcdff69ce3ab44213f7005aef0753f2baa16
sigal <ssamburski@gmail.com>
2020-01-30 18:34:54 +0200

    add Popover to change the item properties, whithout handling the ok

--- src/components/ShoppingItemComponent.js
+++ src/components/ShoppingItemComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './ShoppingItemComponent.css';
-import { Container ,Popover,OverlayTrigger} from 'react-bootstrap';
+import { Container ,Popover,OverlayTrigger,Image,Form,Row,Col} from 'react-bootstrap';
 import imageAddSource from '../images/Add.png'
 
 export default class ShoppingItemComponent extends Component {
@@ -10,24 +10,66 @@ export default class ShoppingItemComponent extends Component {
           this.state = {
             wasChanged:false,
             showChangeItemIcon:false, 
+            fileImg: {
+                file: undefined,
+                URL:undefined
+            }
         };
 
     }
-        HandleClosePopup=(event)=>{
-            this.refs.overlay.handleHide();
+
+    componentDidMount()
+    { const {item}=this.props;
+        if (item.imgFile)
+        {
+            console.log(item);
+            this.setState({
+                fileImg:{
+                    file: item.imgFile,
+                    URL:URL.createObjectURL(item.imgFile.URL.createObjectURL)
+                }
+            })
+        }
         
+    }
+
+    /******************************************** */
+    //Popover functions
+    HandleClosePopup=(event)=>{
+        this.refs.overlay.handleHide();
+    
+    }
+    HandleOkPopup=(event)=>{
+        console.log ("HandleOkPopup ",event.target);
+        this.refs.overlay.handleHide();
+        
+    }
+
+    HandlePropertiesItem=(event)=>{
+        if (this.state.showUpdateProperties===true)
+        {
+            return;
         }
+    }
 
-     
-
-        HandlePropertiesItem=(event)=>{
-            if (this.state.showUpdateProperties===true)
-            {
-                return;
+    handleFileChange=(event)=> {
+        let newFileImg;
+        if (event.target.files[0]) {
+            newFileImg = {
+                file: event.target.files[0],
+                URL: URL.createObjectURL(event.target.files[0])
             }
-            console.log ("HandleButtonItem ",event.target);
+        } else {
+            newFileImg = {
+                file: undefined,
+                URL: undefined
+            }
+        }
+            this.setState({fileImg: newFileImg});        
         }
 
+    /******************************************** */
+    //component functions
         HandleDeleteItem=(index)=>{
             this.props.item.isCompleted=!this.props.item.isCompleted;
                 this.setState({
@@ -36,10 +78,10 @@ export default class ShoppingItemComponent extends Component {
         }
 
       
-        HandleMouseLeave=(event)=>{
-            
+        HandleMouseLeave=(event)=>{         
             this.setState({showChangeItemIcon:false})
         }
+
         handleMouseEnter=(event)=>{
            
             this.setState({showChangeItemIcon:true})
@@ -48,6 +90,7 @@ export default class ShoppingItemComponent extends Component {
         render()
         {
             const { item } = this.props;
+            const {fileImg}=this.state;
             let completedClass = "";
             if (item.isCompleted) {
                 completedClass = "linethrough-text";
@@ -55,24 +98,47 @@ export default class ShoppingItemComponent extends Component {
             else {
                 completedClass = "regular-text";
             }
-
+            let itemText=item.count + ' '+ item.name;
+           
             const popover = 
             (
                 <Popover id="popover-basic" >
-                  <Popover.Title as="h3">עדכון פריט
+                  <Popover.Title as="h3">{item.name} : עדכון    
                         <button onClick={this.HandleClosePopup}>X</button>
                   </Popover.Title>
                   <Popover.Content>
-                    <button>Add image</button>
-                    <button>count</button>
-                    <button onClick={this.HandleClosePopup}>close</button>
+                        <Form.Group>
+                            <Row>
+                                <Col>
+                                    <Form.Label>כמות</Form.Label>
+                                </Col>
+                                <Col>
+                                    <Form.Control type="number" placeholder={item.count} min="1"  />
+                                </Col>
+                            </Row>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>תמונה</Form.Label>
+                            <Row>
+                                <Col>
+                                    <Form.Control type="file" onChange={this.handleFileChange} />
+                                </Col>
+                                <Col>
+                                    <Image src={fileImg.URL} fluid/>
+                                </Col>
+
+                            </Row>
+                        </Form.Group>
+
+                        <button onClick={this.HandleOkPopup}>שנה</button>
+                        <button onClick={this.HandleClosePopup}>סגור</button>
                   </Popover.Content>
           
                 </Popover>
               );
 
             let deleteButtonClass=this.state.showChangeItemIcon===true?"side-left":"hidden"
-            let itemText=item.count + ' '+ item.name
+           
             return (
             <Container className="main-shopping-item" onMouseOver={this.handleMouseEnter} onMouseLeave={this.HandleMouseLeave}  >
                 <p>
