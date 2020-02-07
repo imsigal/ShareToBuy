03ad64114b2fcc339fb8a445ca7344bfec3e4fd6
imsigal <ssamburski@gmail.com>
2020-02-07 20:03:30 +0200

    Updates

---
+++ precache-manifest.8e473bc707265a19b64e9cde1b5ea2bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac22687b8f6326a7fa305d720c29004",
+    "url": "/ShareToBuy/index.html"
+  },
+  {
+    "revision": "9a832e1086e2f159224e",
+    "url": "/ShareToBuy/static/css/main.5e19016c.chunk.css"
+  },
+  {
+    "revision": "bec5ec16a0ee4499723a",
+    "url": "/ShareToBuy/static/js/2.54c46e89.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/ShareToBuy/static/js/2.54c46e89.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a832e1086e2f159224e",
+    "url": "/ShareToBuy/static/js/main.c8644d00.chunk.js"
+  },
+  {
+    "revision": "963e525101defea50d0e",
+    "url": "/ShareToBuy/static/js/runtime-main.16cc1433.js"
+  }
+]);
